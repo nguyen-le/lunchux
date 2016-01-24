84a874fbbd1f282465a2fc0548e19ba7fdb59950
Nguyen Le <nguyentle7@gmail.com>
2016-01-24 01:57:59 -0800

    cookie auth

--- server.js
+++ server.js
@@ -1,36 +1,36 @@
 'use strict';
 
+const cfenv = require('cfenv');
 const Hapi = require('hapi');
-const cfenv = require('cfenv'); // see: https://www.npmjs.com/package/cfenv
+const HapiAuthCookie = require('hapi-auth-cookie');
 const Inert = require('inert');
 
 const loginRoutes = require('./lib/routes/login');
 const userRoutes = require('./lib/routes/user');
+const publicController = require('./lib/routes/public');
 
 var appEnv = cfenv.getAppEnv();
 var server = new Hapi.Server();
 
-server.connection({
-  port: (appEnv.port || 8080)
-});
 
-server.register(Inert, function(err) { if (err) throw err;});
+server.connection({ port: (appEnv.port || 8080) });
+
+server.register(Inert, (err) => { if (err) throw err; });
+server.register(HapiAuthCookie, (err) => {
+  if (err) throw err;
+  server.auth.strategy('session', 'cookie', {
+    password: 'c4n74ver',
+    cookie: 'sid',
+    isSecure: false
+  });
+});
 
 // static files
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: {
-    file: './public/index.html'
-  }
-});
-server.route({
-  method: 'GET',
-  path: '/public/bundle.js',
-  handler: {
-    file: './public/bundle.js'
-  }
-});
+server.route(
+  { method: 'GET', path: '/', config: publicController.indexHtml },
+  { method: 'GET', path: '/public/bundle.js', config: publicController.bundleJs }
+);
+
 server.route(loginRoutes);
 server.route(userRoutes);
 
