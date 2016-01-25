a6d3bebed9d4f9fa9b52ee3dad1589dcba60b72b
Nguyen Le <nguyentle7@gmail.com>
2016-01-24 20:39:27 -0800

    material for login

--- public/js/components/login/login_form.jsx
+++ public/js/components/login/login_form.jsx
@@ -1,32 +1,59 @@
 import React from 'react';
+//import Divider from 'material-ui/lib/divider';
+import Color from 'material-ui/lib/styles/colors';
+import RaisedButton from 'material-ui/lib/raised-button';
+import TextField from 'material-ui/lib/text-field';
 
 
 const LoginForm = React.createClass({
   getInitialState: function() {
-    return {email: '', password: ''};
+    return {email: '', password: '', errors: new Map};
+  },
+  render: function() {
+    const emailError = this.state.errors.get('email');
+    const passwordError = this.state.errors.get('password');
+
+    return (
+      <div>
+        <form onSubmit={this._onSubmit}>
+          <TextField
+            onChange={this._onChangeEmail}
+            type='text'
+            floatingLabelStyle={ emailError ? {color: Color.red500} : null}
+            floatingLabelText='Email'
+            errorText={ emailError ? 'made mistake' : null }
+            autoFocus={true}/>
+          <br />
+          <TextField
+            onChange={this._onChangePassword}
+            type='password'
+            floatingLabelStyle={ passwordError ? {color: Color.red500} : null }
+            floatingLabelText='Password'
+            errorText={ passwordError ? passwordError : null }
+          />
+          <br />
+          <RaisedButton
+            onClick={this._onSubmit}
+            label='Log In'
+            backgroundColor={Color.red100}/>
+        </form>
+      </div>
+    );
   },
   _onChangeEmail: function(event) {
     this.setState({email: event.target.value});
   },
-  _onChangePw: function(event) {
+  _onChangePassword: function(event) {
     this.setState({password: event.target.value});
-  },
-  render: function() {
-    return (
-      <form onSubmit={this._onSubmit}>
-        <p>Login form</p>
-        <label htmlFor='login-email'>Email Address</label>
-        <input id='login-email' type='text' value={this.state.email} onChange={this._onChangeEmail}/>
-        <label htmlFor='login-pw'>Password</label>
-        <input id='login-pw' type='password' value={this.state.password} onChange={this._onChangePw}/>
-        <input type='submit' />
-      </form>
-    );
   },
   _onSubmit: function(event) {
     event.preventDefault();
 
-    let xhr = new XMLHttpRequest();
+    const payload = {
+      email: this.state.email,
+      password: this.state.password
+    };
+    const xhr = new XMLHttpRequest();
     xhr.open('POST', '/login');
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.onreadystatechange = () => {
@@ -36,7 +63,7 @@ const LoginForm = React.createClass({
         // signal error messaging
       }
     };
-    xhr.send(JSON.stringify(this.state));
+    xhr.send(JSON.stringify(payload));
   }
 });
 
